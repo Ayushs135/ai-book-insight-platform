779996beb955183723798c406b3812f40b94525b
Ayushs135 <ayushs01032005@gmail.com>
2026-04-18 11:54:25 +0530

    CORS Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'books',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -55,9 +56,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'backend.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
